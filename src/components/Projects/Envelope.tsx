9ce7ff8e78ad83a6a9df20227d72ac76655f1e67
Daryl Napp <daryl.napp@washpost.com>
2025-04-05 01:11:34 -0400

    add palceholder data

--- src/components/Projects/Envelope.tsx
+++ src/components/Projects/Envelope.tsx
@@ -3,140 +3,99 @@ import React from "react";
 export const Envelope = () => {
   return (
     <svg
-      viewBox="85 0 1130 1010"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="85 0 1130 1010"
     >
-      <rect x="87" y="315" width="1124" height="695" fill="white" />
-      <g filter="url(#filter0_d_101_2)">
+      <path fill="#fff" d="M87 315h1124v695H87z" />
+      <g filter="url(#a)">
         <path
-          d="M635.819 7.66172C644.319 2.90172 654.682 2.90169 663.181 7.6617L1118.36 262.57C1143.53 276.662 1133.52 315 1104.68 315H194.32C165.478 315 155.475 276.662 180.639 262.57L635.819 7.66172Z"
-          fill="white"
+          fill="#fff"
+          d="M636 8c8-5 19-5 27 0l455 255c26 14 16 52-13 52H194c-29 0-39-38-13-52L636 8Z"
         />
       </g>
-      <path d="M411.017 134L735.017 315H87.0166L411.017 134Z" fill="white" />
-      <path d="M888 134L1212 315H564L888 134Z" fill="white" />
-      <g filter="url(#filter1_i_101_2)">
-        <g filter="url(#filter2_i_101_2)">
+      <path fill="#fff" d="m411 134 324 181H87l324-181Z" />
+      <path fill="#fff" d="m888 134 324 181H564l324-181Z" />
+      <g filter="url(#b)">
+        <g filter="url(#c)">
           <path
-            d="M636.13 585.909C643.93 589.749 653.071 589.749 660.87 585.909L1103.13 368.119C1129.72 355.026 1120.4 315 1090.76 315H206.239C176.6 315 167.28 355.026 193.869 368.119L636.13 585.909Z"
             fill="#D9D9D9"
+            d="M636 586c8 4 17 4 25 0l442-218c27-13 17-53-12-53H206c-29 0-39 40-12 53l442 218Z"
           />
         </g>
-        <path
-          d="M411.008 474L735.008 315H87.0083L411.008 474Z"
-          fill="#D9D9D9"
-        />
-        <path
-          d="M888.008 474L1212.01 315H564.008L888.008 474Z"
-          fill="#D9D9D9"
-        />
+        <path fill="#D9D9D9" d="m411 474 324-159H87l324 159Z" />
+        <path fill="#D9D9D9" d="m888 474 324-159H564l324 159Z" />
       </g>
       <defs>
         <filter
-          id="filter0_d_101_2"
-          x="150.274"
-          y="4.09167"
-          width="998.452"
-          height="342.908"
-          filterUnits="userSpaceOnUse"
+          id="a"
+          width="998.5"
+          height="342.9"
+          x="150.3"
+          y="4.1"
           color-interpolation-filters="sRGB"
+          filterUnits="userSpaceOnUse"
         >
           <feFlood flood-opacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
             result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
           />
           <feOffset dy="16" />
           <feGaussianBlur stdDeviation="8" />
           <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_101_2" />
           <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_101_2"
-          />
-          <feBlend
-            mode="normal"
             in="SourceGraphic"
             in2="effect1_dropShadow_101_2"
             result="shape"
           />
         </filter>
         <filter
-          id="filter1_i_101_2"
-          x="87.0083"
-          y="299"
+          id="b"
           width="1125"
-          height="289.789"
-          filterUnits="userSpaceOnUse"
+          height="289.8"
+          x="87"
+          y="299"
           color-interpolation-filters="sRGB"
+          filterUnits="userSpaceOnUse"
         >
           <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
           <feColorMatrix
             in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
             result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
           />
           <feOffset dy="-16" />
           <feGaussianBlur stdDeviation="8" />
-          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="shape"
-            result="effect1_innerShadow_101_2"
-          />
+          <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend in2="shape" result="effect1_innerShadow_101_2" />
         </filter>
         <filter
-          id="filter2_i_101_2"
-          x="178.197"
+          id="c"
+          width="944.6"
+          height="277.8"
+          x="178.2"
           y="315"
-          width="944.605"
-          height="277.789"
-          filterUnits="userSpaceOnUse"
           color-interpolation-filters="sRGB"
+          filterUnits="userSpaceOnUse"
         >
           <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
           <feColorMatrix
             in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
             result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
           />
           <feOffset dx="4" dy="4" />
           <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="shape"
-            result="effect1_innerShadow_101_2"
-          />
+          <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend in2="shape" result="effect1_innerShadow_101_2" />
         </filter>
       </defs>
     </svg>
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { Envelope } from "./Envelope";
 
+const projects = [
+  {
+    title: "dbnapp.com",
+    thumbnail: "",
+    description:
+      "My site! Built using Typescript, React, Tailwind CSS, and Vite. Maybe you've seen it idk. Previous version of this site I wanted to be kind of minimal, even foregoing using any frameworks. Really flex my core js and css knowledge. This time I just wanted to make something quicker and more interesting. So I did! ",
+  },
+];
+
 export const Projects = () => {
   return (
     <div className="w-[95%] lg:w-[70rem]">
+      <div className="flex flex-col gap-4">
+        {projects.map((project, index) => (
+          <div key={index}>
+            <div className="font-[Damion]">{project.title}</div>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
       <Envelope />
     </div>
   );
