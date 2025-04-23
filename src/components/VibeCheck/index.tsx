7ac26a8025a49f5d9087fd0b7e3ad2fb35ac92b4
Daryl Napp <daryl.napp@washpost.com>
2025-04-23 02:12:34 -0400

    vibe-check animation

--- src/components/VibeCheck/index.tsx
+++ src/components/VibeCheck/index.tsx
@@ -1,34 +1,64 @@
 import { useAtomValue } from "jotai";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { musicAtom, soundsAtom } from "../Sounds/atoms";
 import { animate, AnimationPlaybackControls } from "motion";
+import { motion } from "motion/react";
+
+const absoluteButtonAnimateProperties = {
+  absolutely: {
+    translateX: "6rem",
+  },
+  not: { translateX: "-14rem", visibility: "hidden", opacity: 0 },
+  "": { translateX: "0rem" },
+} as const;
+
+const notButtonAnimateProperties = {
+  absolutely: {
+    translateX: "6rem",
+    visibility: "hidden",
+    opacity: 0,
+  },
+  not: { translateX: "-14rem" },
+  "": { translateX: "0rem" },
+} as const;
 
 export const VibeCheck = () => {
   const sounds = useAtomValue(soundsAtom);
   const music = useAtomValue(musicAtom);
-  const animateRef = useRef<AnimationPlaybackControls>(null);
+  const volumeAnimateRef = useRef<AnimationPlaybackControls>(null);
+  const [currentButtonFocus, setCurrentButtonFocus] = useState<
+    "absolutely" | "not"
+  >("absolutely");
+  const [choiceSelected, setChoiceSelected] = useState<
+    "absolutely" | "not" | ""
+  >("");
 
-  const stopAnimation = () => {
-    if (animateRef.current) {
-      animateRef.current.stop();
+  const stopVolumeAnimation = () => {
+    if (volumeAnimateRef.current) {
+      volumeAnimateRef.current.stop();
     }
   };
 
   return (
     <div className="font-[Damion] mt-96 flex flex-col items-center justify-center">
       <div className="text-3xl md:text-5xl">Vibes on?</div>
-      <div className="flex gap-6">
-        <button
+      <div className="relative flex gap-6">
+        <motion.button
+          key="absolutely-button"
           className="text-5xl md:text-8xl"
+          animate={{
+            ...absoluteButtonAnimateProperties[choiceSelected],
+          }}
           onClick={() => {
-            stopAnimation();
+            setChoiceSelected("absolutely");
+            stopVolumeAnimation();
 
             sounds?.setVolume(0);
             sounds?.unMute();
             music?.setVolume(0);
             music?.unMute();
 
-            animateRef.current = animate(0, 100, {
+            volumeAnimateRef.current = animate(0, 100, {
               duration: 10,
               onUpdate: (v) => {
                 sounds?.setVolume(v);
@@ -36,16 +66,54 @@ export const VibeCheck = () => {
               },
             });
           }}
+          onFocus={() => {
+            setCurrentButtonFocus("absolutely");
+          }}
+          onMouseOver={() => {
+            setCurrentButtonFocus("absolutely");
+          }}
         >
-          Absolutely!
-        </button>
-        <button
-          className="text-5xl md:text-8xl"
+          <motion.span
+            animate={{
+              color:
+                choiceSelected === "absolutely"
+                  ? [
+                      "#ff00ff",
+                      "#00ffff",
+                      "#00ff00",
+                      "#ffff00",
+                      "#ff0000",
+                      "#ff00ff",
+                    ]
+                  : [],
+            }}
+            transition={{
+              repeat: Infinity,
+              duration: 2,
+            }}
+          >
+            Absolutely
+            <motion.span
+              animate={{
+                opacity: currentButtonFocus === "absolutely" ? 1 : 0,
+              }}
+            >
+              !
+            </motion.span>
+          </motion.span>
+        </motion.button>
+        <motion.button
+          key="not-button"
+          className=" text-5xl md:text-8xl"
+          animate={{
+            ...notButtonAnimateProperties[choiceSelected],
+          }}
           onClick={() => {
-            stopAnimation();
+            setChoiceSelected("not");
+            stopVolumeAnimation();
             const initVol = sounds?.getVolume() || 100;
 
-            animateRef.current = animate(initVol, 0, {
+            volumeAnimateRef.current = animate(initVol, 0, {
               duration: 5,
               onUpdate: (v) => {
                 sounds?.setVolume(v);
@@ -53,9 +121,56 @@ export const VibeCheck = () => {
               },
             });
           }}
+          onFocus={() => {
+            setCurrentButtonFocus("not");
+          }}
+          onMouseOver={() => {
+            setCurrentButtonFocus("not");
+          }}
         >
-          Not!
-        </button>
+          Not
+          <motion.span
+            animate={{
+              opacity: currentButtonFocus === "not" ? 1 : 0,
+            }}
+          >
+            !
+          </motion.span>
+        </motion.button>
+
+        {/* pointer animates separately between moving between options and repeatedly up and down, thus two divs */}
+        <motion.div
+          key="pointer"
+          className="absolute top-24"
+          animate={{
+            left: currentButtonFocus === "absolutely" ? "10rem" : "30rem",
+            visibility: choiceSelected ? "hidden" : "visible",
+            opacity: choiceSelected ? 0 : 1,
+          }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+          }}
+        >
+          <motion.div
+            className="text-5xl pointer-events-none"
+            animate={{
+              translateY: ["0%", "100%"],
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "mirror",
+              duration: 0.75,
+            }}
+            style={{
+              left: currentButtonFocus === "absolutely" ? "10rem" : "30rem",
+            }}
+          >
+            👆
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,17 @@
+import React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { Greetings } from "../components/Greetings";
+import { VibeCheck } from "../components/VibeCheck";
+import { Projects } from "../components/Projects";
+import { Postcard } from "../components/Postcard";
 
 export const Route = createFileRoute("/")({
-  component: Greetings,
+  component: () => (
+    <>
+      <Greetings />
+      <VibeCheck />
+      <Postcard />
+      <Projects />
+    </>
+  ),
 });
