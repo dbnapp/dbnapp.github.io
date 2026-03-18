ea1cce418db67d985c418a1cecea5064c324e569
Daryl Napp <daryl.napp@washpost.com>
2026-03-18 16:26:17 -0400

    fix stamp url loading

--- src/components/Stamp.tsx
+++ src/components/Stamp.tsx
@@ -1,12 +1,11 @@
+import stampUrl from "/src/assets/stamp.webp";
+
 export const Stamp = ({ className }: { className?: string }) => {
   return (
     <div
       className={`aspect-[3/4] overflow-hidden opacity-60 ${className ?? ""}`}
     >
-      <img
-        src={new URL("../assets/stamp.webp", import.meta.url).toString()}
-        className="w-36"
-      />
+      <img src={stampUrl} className="w-36" />
     </div>
   );
 };
